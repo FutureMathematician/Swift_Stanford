87689bfb4c6639358fb2926c389594b0afea9d78
Haoran <bofengco@foxmail.com>
2020-12-12 20:34:39 +0800

    Create MemoryGame.swift

---
+++ MemoryGame.swift
@@ -0,0 +1,32 @@
+//
+//  MemoryGame.swift
+//  stanford001
+//
+//  Created by Haoran Liu on 2020/12/9.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(card: Card){
+        print("card chosen: \(card)")
+    }
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairsOfCards{
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
